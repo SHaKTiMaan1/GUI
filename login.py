a253c5639cd4f9d363a15e5b42c3ce11a9b51637
redkirti <49024860+redkirti@users.noreply.github.com>
2020-07-17 12:51:47 +0530

    Datasets creation

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ import bcrypt
 import re
 from getpass import getpass
 import pymongo
+import sqlite3
 """Connection String"""
 client = pymongo.MongoClient(
     "mongodb+srv://CCI:root@cluster0.4gzmr.mongodb.net/CARE?retryWrites=true&w=majority")
@@ -18,10 +19,14 @@ doc = col.find(query)
 for x in doc:
     hashpass = x["password"]
     hashpass = hashpass.encode('utf-8')
+    cci_id = x["cci_id"]
 """Validating Password"""
 pswd = pswd.encode('utf-8')
 if(bcrypt.checkpw(pswd, hashpass)):
     print("Login Successful")
+    f = open("CCI_ID.txt","w+")
+    f.write(cci_id)
+    f.close()
 else:
     print("Wrong Credentials")
     
--- sync.py
+++ sync.py
@@ -1,6 +1,5 @@
 import pymongo
 import sqlite3
-from login import cci_id
 
 client_web = pymongo.MongoClient(
     "mongodb+srv://CCI:root@cluster0.4gzmr.mongodb.net/Jhansi?retryWrites=true&w=majority")
@@ -9,20 +8,15 @@ col = db["children"]
 
 conn = sqlite3.connect("child.db")
 c = conn.cursor()
-c = conn.execute('''SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'details' ;''')
 
-if c.fetchone()[0] != 'details':
-    conn.execute('''CREATE TABLE details 
-                    (C_ID INT PRIMARY KEY NOT NULL, 
-                    NAME TEXT NOT NULL, 
-                    AGE INT NOT NULL, 
-                    DOR TEXT NOT NULL,
-                    GENDER CHAR(1) NOT NULL,
-                    WITNESS CHAR(25) NOT NULL); ''')
-else:
-    pass
-    
-
+c.execute('''CREATE TABLE IF NOT EXISTS details 
+            (C_ID INT PRIMARY KEY NOT NULL, 
+            NAME TEXT NOT NULL, 
+            AGE INT NOT NULL, 
+            DOR TEXT NOT NULL,
+            GENDER CHAR(1) NOT NULL,
+            WITNESS CHAR(25) NOT NULL,
+            SET_EXIST BOOLEAN);''')  
 
 c = conn.execute('''SELECT C_ID FROM details;''')
 rows = c.fetchall()
